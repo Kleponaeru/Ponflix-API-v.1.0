44491677ffb2d96998ec881641ef919fcacce262
Kleponaeru <kelvinlie08@gmail.com>
2026-07-03 19:32:15 +0700

    remove route wrong import

--- src/lib/kuramanime/anime.ts
+++
@@ -1,7 +0,0 @@
-import type { AnimeDetails } from "@/types/anime";
-import { parseAnime } from "./parser/anime";
-
-export async function getAnime(slug: string): Promise<AnimeDetails | null> {
-  void slug;
-  return parseAnime("");
-}
